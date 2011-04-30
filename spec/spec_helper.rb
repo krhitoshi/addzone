c35391fc5e2af60bda8aadbc35e691aafdec8338
Hitoshi Kurokawa <hitoshi@nextseed.jp>
2011-04-30 19:50:57 +0900

    remove backup files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,6 +9,8 @@ require 'tmpdir'
 
 def test_init
   Dir.chdir File.dirname(__FILE__)
+  FileUtils.rm(Dir.glob("data/etc/backup/*"))
+  FileUtils.rm(Dir.glob("data/master/backup/*"))
   File.delete "data/etc/hosting.conf" if File.exist?("data/etc/hosting.conf")
   FileUtils.copy_file "data/etc/hosting.conf.dist", "data/etc/hosting.conf", true
 end
