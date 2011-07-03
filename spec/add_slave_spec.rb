2f579b3cbf19b8acdee20a567f6847da19c24dd2
Hitoshi Kurokawa <hitoshi@nextseed.jp>
2011-07-03 20:53:31 +0900

    use nest describe

--- spec/add_slave_spec.rb
+++ spec/add_slave_spec.rb
@@ -2,19 +2,21 @@
 
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe AddSlave, "正常なコンフィグファイルの場合" do
+describe AddSlave do
   before :all do
     test_init
   end
   after :all do
     test_end
   end
-  before do
-    @add_slave = AddSlave.new("etc/addzone.conf")
-  end
-  it { @add_slave.master_ip.should == "192.168.1.1" }
-  it "コンフィグファイルの設定テキストが正しいこと" do
-    conf = <<EOS
+
+  describe AddSlave, "正常なコンフィグファイルの場合" do
+    before do
+      @add_slave = AddSlave.new("etc/addzone.conf")
+    end
+    it { @add_slave.master_ip.should == "192.168.1.1" }
+    it "コンフィグファイルの設定テキストが正しいこと" do
+      conf = <<EOS
 // example.com : 20110425150015
 zone "example.com" {
       type slave;
@@ -23,26 +25,20 @@ zone "example.com" {
 };
 
 EOS
-    @add_slave.zone_conf("example.com").should == conf
+      @add_slave.zone_conf("example.com").should == conf
+    end
   end
-end
 
-describe AddSlave, "マスターIPアドレスを変更した場合" do
-  before :all do
-    test_init
-  end
-  after :all do
-    test_end
-  end
-  before do
-    @add_slave = AddSlave.new("etc/addzone.conf")
-    @add_slave.master_ip = "192.168.100.1"
-  end
-  it "正しく設定されていること" do
-    @add_slave.master_ip.should == "192.168.100.1"
-  end
-  it "コンフィグファイルの設定テキストが正しいこと" do
-    conf = <<EOS
+  describe AddSlave, "マスターIPアドレスを変更した場合" do
+    before do
+      @add_slave = AddSlave.new("etc/addzone.conf")
+      @add_slave.master_ip = "192.168.100.1"
+    end
+    it "正しく設定されていること" do
+      @add_slave.master_ip.should == "192.168.100.1"
+    end
+    it "コンフィグファイルの設定テキストが正しいこと" do
+      conf = <<EOS
 // example.com : 20110425150015
 zone "example.com" {
       type slave;
@@ -51,33 +47,27 @@ zone "example.com" {
 };
 
 EOS
-    @add_slave.zone_conf("example.com").should == conf
+      @add_slave.zone_conf("example.com").should == conf
+    end
   end
-end
 
-
-describe AddSlave, "ゾーンを削除する場合" do
-  before :all do
-    test_init
-  end
-  after :all do
-    test_end
-  end
-  before do
-    @add_slave = AddSlave.new("etc/addzone.conf")
-    clear_files
-    @add_slave.delete_zone("example.jp")
-  end
-  it "元のゾーンファイルが存在しないこと" do
-    File.should_not be_exist("slave/example.jp.zone")
-  end
-  it "バックアップファイルが保存されていること" do
-    File.should be_exist("slave/backup/example.jp.zone")
-  end
-  it "コンフィグファイルのバックアップが保存されていること" do
-    File.should be_exist("etc/backup/hosting.conf.20110425150015")
-  end
-  it "コンフィグファイルからゾーンの設定を削除できていること" do
-    lambda{ @add_slave.delete_zone_check("example.jp") }.should raise_error AddZone::ConfigureError
+  describe AddSlave, "ゾーンを削除する場合" do
+    before do
+      @add_slave = AddSlave.new("etc/addzone.conf")
+      clear_files
+      @add_slave.delete_zone("example.jp")
+    end
+    it "元のゾーンファイルが存在しないこと" do
+      File.should_not be_exist("slave/example.jp.zone")
+    end
+    it "バックアップファイルが保存されていること" do
+      File.should be_exist("slave/backup/example.jp.zone")
+    end
+    it "コンフィグファイルのバックアップが保存されていること" do
+      File.should be_exist("etc/backup/hosting.conf.20110425150015")
+    end
+    it "コンフィグファイルからゾーンの設定を削除できていること" do
+      lambda{ @add_slave.delete_zone_check("example.jp") }.should raise_error AddZone::ConfigureError
+    end
   end
 end
--- spec/add_zone_spec.rb
+++ spec/add_zone_spec.rb
@@ -2,47 +2,44 @@
 
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe AddZone, "正常なコンフィグファイルの場合" do
+describe AddZone do
   before :all do
     test_init
   end
   after :all do
     test_end
   end
-  before do
-    @add_zone = AddZone.new("etc/addzone.conf")
-    clear_files
-  end
-  it "コンフィグファイルにないゾーンが検出できること" do
-    lambda{ @add_zone.add_zone_check("example.com") }.should_not raise_error
-  end
-  it "コンフィグファイルにあるゾーンが検出できること" do
-    lambda{ @add_zone.add_zone_check("example.jp") }.should raise_error AddZone::ConfigureError
-  end
-  it "空白の量が違っても認識できること" do
-    lambda{ @add_zone.add_zone_check("example.net") }.should raise_error AddZone::ConfigureError
-  end
-  it "空白にタブが使われていても認識できること" do
-    lambda{ @add_zone.add_zone_check("example.info") }.should raise_error AddZone::ConfigureError
-  end
-end
 
-describe AddZone, "各種存在しないファイルパスが指定されている場合" do
-  before :all do
-    test_init
+  describe AddZone, "正常なコンフィグファイルの場合" do
+    before do
+      @add_zone = AddZone.new("etc/addzone.conf")
+      clear_files
+    end
+    it "コンフィグファイルにないゾーンが検出できること" do
+      lambda{ @add_zone.add_zone_check("example.com") }.should_not raise_error
+    end
+    it "コンフィグファイルにあるゾーンが検出できること" do
+      lambda{ @add_zone.add_zone_check("example.jp") }.should raise_error AddZone::ConfigureError
+    end
+    it "空白の量が違っても認識できること" do
+      lambda{ @add_zone.add_zone_check("example.net") }.should raise_error AddZone::ConfigureError
+    end
+    it "空白にタブが使われていても認識できること" do
+      lambda{ @add_zone.add_zone_check("example.info") }.should raise_error AddZone::ConfigureError
+    end
   end
-  after :all do
-    test_end
-  end
-  it "コンストラクタでエラーを発生すること" do
-    lambda{ AddZone.new("etc/addzone_not_exist.conf") }.should raise_error
-  end
-end
 
-describe AddZone, "存在しないコンフィグファイル" do
-  before do
+  describe AddZone, "各種存在しないファイルパスが指定されている場合" do
+    it "コンストラクタでエラーを発生すること" do
+      lambda{ AddZone.new("etc/addzone_not_exist.conf") }.should raise_error
+    end
   end
-  it "エラーになる" do
-    lambda{ @add_zone = AddZone.new("not_exist.conf") }.should raise_error
+
+  describe AddZone, "存在しないコンフィグファイル" do
+    before do
+    end
+    it "エラーになる" do
+      lambda{ @add_zone = AddZone.new("not_exist.conf") }.should raise_error
+    end
   end
 end
