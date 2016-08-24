50699d962d818895c942455a84085356a3ac7103
Hitoshi Kurokawa <hitoshi@nextseed.jp>
2016-08-24 15:51:26 +0900

    AddZone.check_dns

--- lib/addzone.rb
+++ lib/addzone.rb
@@ -4,4 +4,15 @@ require 'addzone/base'
 
 module AddZone
   DEFAULT_ADDZONE_CONF_PATH = "/etc/addzone.conf"
+
+  def self.check_dns(server, domain)
+    result = `dig @#{server} #{domain} +norec`
+    line = result.lines.grep(/ANSWER: \d+,/)
+    line[0] =~ /ANSWER: (\d+),/
+    if $1 == "0"
+      puts "#{server} NG"
+    else
+      puts "#{server} OK"
+    end
+  end
 end
